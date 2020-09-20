b18604e9938f372c3afbbb0e7c7a05d926bd0faa
Moses Kamara <kamara.moses@yahoo.com>
2020-09-20 02:47:31 -0700

    changes to my connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 // Import MySQL connection.
 const mysql = require('mysql');
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        port: '3306',
-        user: 'root',
-        password: 'KyMyAr091217$',
-        database: 'burgers_db'
-    });
-};
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'KyMyAr091217$',
+    database: 'burgers_db'
+});
+
 
 //Make connection
 connection.connect((err) => {
-    if(err){
+    if (err) {
         console.log('error connecting:' + err.stack)
         return
     }
