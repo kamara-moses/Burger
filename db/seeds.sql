3f61e68e624b43c65d5260a09319f5e42b9c5abf
Moses Kamara <kamara.moses@yahoo.com>
2020-09-20 00:29:27 -0700

    created db folder with schema.sql and seeds.sql files for MySQL  database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('CHEESEBURGER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HAMBURGER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('CHICKEN BURGER', false);
