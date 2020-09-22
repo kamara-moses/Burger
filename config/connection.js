c963327a2f21aa9142bcd365628b3ab3f9d78f52
Moses Kamara <kamara.moses@yahoo.com>
2020-09-22 14:17:38 -0700

    updated my connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Import MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: 'root',
         password: 'KyMyAr091217$',
         database: 'burgers_db'
-});
-
+    });
+};
 
 // Make connection.
 connection.connect((err) => {
@@ -19,5 +24,6 @@ connection.connect((err) => {
   console.log('connected as id ' + connection.threadId);
 });
 
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
